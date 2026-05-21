5c36277efe51ddb6ccc5992a969b2af82af67f55
bhavsarrenuka26 <bhavsarrenuka12@gmail.com>
2026-05-21 14:32:13 +0530

    Start with designing home page

--- app/page.js
+++ app/page.js
@@ -1,65 +1,64 @@
 import Image from "next/image";
 
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <>
+   <div className="flex justify-center items-center flex-col gap-4 text-white h-[40vh]">
+    <div className="font-bold text-5xl flex gap-1 justify-center items-center">Buy Me a Chai <span><img className="invertImg" src="/tea.gif" width={88} alt="" /></span></div>
+    <p>
+      A crowdfunding playform for creators. Get funded by your fans and followers. Start Now!
+    </p>
+    <div>
+     <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
+    <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center leading-5">Read More</button>
     </div>
+   </div>
+   <div className="bg-white h-1 opacity-15">
+   </div>
+   <div className="text-white container mx-auto pb-28 pt-14">
+    <h2 className="text-3xl font-bold text-center mb-14">
+      Your Fans can buy you a Chai
+    </h2>
+    <div className="flex gap-5 justify-around">
+      <div className="item space-y-3 flex flex-col items-center justify-center">
+        <img className=" bg-slate-400 rounded-full p-2 text-black" src="/man.gif" width={88} alt="" />
+        <p className="font-bold">Fund Yourself</p>
+          <p className=" text-center">Your fans are available for to help you</p>
+      </div>
+       <div className="item space-y-3 flex flex-col items-center justify-center">
+        <img className=" bg-slate-400 rounded-full p-2 text-black" src="/coin.gif" width={88} alt="" />
+        <p className="font-bold">Fund Yourself</p>
+          <p className=" text-center">Your fans are available for to help you</p>
+      </div>
+       <div className="item space-y-3 flex flex-col items-center justify-center">
+        <img className=" bg-slate-400 rounded-full p-2 text-black" src="/group.gif" width={88} alt="" />
+        <p className="font-bold">Your Fans want to help</p>
+        <p className=" text-center">Your fans are available for to help you</p>
+      </div>
+    </div>
+   </div>
+   <div className="bg-white h-1 opacity-15">
+   </div>
+  <div className="text-white container mx-auto pb-28 pt-14 flex flex-col items-center justify-center">
+  
+  <h2 className="text-3xl font-bold text-center mb-10">
+    Learn more about us
+  </h2>
+
+  <div className="w-full max-w-2xl aspect-video">
+    <iframe
+      className="w-full h-full rounded-xl shadow-lg"
+      src="https://www.youtube.com/embed/ojuUnfqnUI0?si=wMUv4DG3ia6Wt4zn"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+    ></iframe>
+  </div>
+
+</div>
+   <div className="bg-white h-1 opacity-15"></div>
+   </>
   );
 }
