048a070dfd944b8311072bfbbe8630c0e569376c
bhavsarrenuka26 <bhavsarrenuka12@gmail.com>
2026-05-24 16:19:21 +0530

    Dynamic year

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear()
   return (
-   <footer className="bg-gray-900 text-white flex justify-center px-4 h-16 items-center">
-    <p className='text-center'>
-        Copyright &copy; Get me A Chai - All rights reserved!
-    </p>
-   </footer>
+   <footer className='bg-gray-900 text-white flex items-center justify-center px-4 h-16'>
+        <p className='text-center'>Copyright &copy; {currentYear} Get me A Chai - All rights reserved!</p>
+    </footer>
   )
 }
 
