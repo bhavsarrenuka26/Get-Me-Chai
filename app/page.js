241ecf1382f815af18cd356d5dc4431276fedf7c
bhavsarrenuka26 <bhavsarrenuka12@gmail.com>
2026-06-16 22:37:01 +0530

    update info

--- app/page.js
+++ app/page.js
@@ -4,11 +4,16 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-   <div className="flex justify-center items-center flex-col gap-4 text-white h-[40vh]">
-    <div className="font-bold text-5xl flex gap-1 justify-center items-center">Buy Me a Chai <span><img className="invertImg" src="/tea.gif" width={88} alt="" /></span></div>
-    <p>
-      A crowdfunding playform for creators. Get funded by your fans and followers. Start Now!
-    </p>
+   <div className="flex justify-center items-center flex-col gap-4 text-white h-[40vh] px-5 md:px-0 text-xs md:text-base">
+    <div className="font-bold md:text-5xl text-xl flex gap-1 justify-center items-center">Get Me a Chai <span><img className="invertImg" src="/tea.gif" width={88} alt="" /></span></div>
+    <p className="text-center md:text-left">
+          A crowdfunding platform for creators to fund their projects. 
+          
+        </p>
+        <p className="text-center md:text-left">
+
+          A place where your fans can buy you a chai. Unleash the power of your fans and get your projects funded.
+        </p>
     <div>
       <Link href={"/login"}>
      <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button></Link>
@@ -18,26 +23,24 @@ export default function Home() {
    </div>
    <div className="bg-white h-1 opacity-15">
    </div>
-   <div className="text-white container mx-auto pb-28 pt-14">
-    <h2 className="text-3xl font-bold text-center mb-14">
-      Your Fans can buy you a Chai
-    </h2>
-    <div className="flex gap-5 justify-around">
-      <div className="item space-y-3 flex flex-col items-center justify-center">
-        <img className=" bg-slate-400 rounded-full p-2 text-black" src="/man.gif" width={88} alt="" />
-        <p className="font-bold">Fund Yourself</p>
-          <p className=" text-center">Your fans are available for to help you</p>
-      </div>
-       <div className="item space-y-3 flex flex-col items-center justify-center">
-        <img className=" bg-slate-400 rounded-full p-2 text-black" src="/coin.gif" width={88} alt="" />
-        <p className="font-bold">Fund Yourself</p>
-          <p className=" text-center">Your fans are available for to help you</p>
-      </div>
-       <div className="item space-y-3 flex flex-col items-center justify-center">
-        <img className=" bg-slate-400 rounded-full p-2 text-black" src="/group.gif" width={88} alt="" />
-        <p className="font-bold">Your Fans want to help</p>
-        <p className=" text-center">Your fans are available for to help you</p>
-      </div>
+   <div className="text-white container mx-auto pb-28 pt-14 px-10">
+     <h2 className="text-3xl font-bold text-center mb-14">Your Fans can buy you a Chai</h2>
+  <div className="flex gap-5 justify-around">
+    <div className="item space-y-3 flex flex-col items-center justify-center">
+      <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/man.gif" alt="" />
+      <p className="font-bold text-center">Fans want to help</p>
+      <p className="text-center">Your fans are available to support you</p>
+    </div>
+    <div className="item space-y-3 flex flex-col items-center justify-center">
+      <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/coin.gif" alt="" />
+      <p className="font-bold text-center">Fans want to contribute</p>
+      <p className="text-center">Your fans are willing to contribute financially</p>
+    </div>
+    <div className="item space-y-3 flex flex-col items-center justify-center">
+      <img className="bg-slate-400 rounded-full p-2 text-black" width={88} src="/group.gif" alt="" />
+      <p className="font-bold text-center">Fans want to collaborate</p>
+      <p className="text-center">Your fans are ready to collaborate with you</p>
+    </div>
     </div>
    </div>
    <div className="bg-white h-1 opacity-15">
